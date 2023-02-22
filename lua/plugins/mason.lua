230527699dd0a000017f0b0b0cc283a37caa2e7a
Siposattila <sattipolo@gmail.com>
2023-02-22 21:36:41 +0100

    mason conf

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,19 @@
+return {
+    "williamboman/mason.nvim",
+    opts = {
+        ensure_installed = {
+            "json-lsp",
+            "lua-language-server",
+            "shellcheck",
+            "pyright",
+            "css-lsp",
+            "phpactor",
+            "jdtls",
+            "omnisharp",
+            "rust-analyzer",
+            "typescript-language-server",
+            "vue-language-server",
+            "yaml-language-server",
+        },
+    },
+}
