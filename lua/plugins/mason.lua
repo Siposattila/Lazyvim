7ecd31b9dc284352b9c8af278ea1e0315f94b0c5
Sipos Attila <sattipolo@gmail.com>
2023-02-27 08:38:50 +0100

    This happens when you are new

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,19 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "json-lsp",
+      "lua-language-server",
+      "pyright",
+      "css-lsp",
+      "intelephense",
+      "jdtls",
+      "omnisharp",
+      "rust-analyzer",
+      "typescript-language-server",
+      "vetur-vls",
+      "yaml-language-server",
+      "gopls",
+    },
+  },
+}
