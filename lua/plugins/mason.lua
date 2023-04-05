868b1ed446702b39964769553c61c2785f5cf52c
Sipos Attila <sattipolo@gmail.com>
2023-04-05 17:11:25 +0200

    omnisharp handler with fixed semantic tokens

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,19 +1,262 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "json-lsp",
-      "lua-language-server",
-      "pyright",
-      "css-lsp",
-      "intelephense",
-      "jdtls",
-      "omnisharp",
-      "rust-analyzer",
-      "typescript-language-server",
-      "vetur-vls",
-      "yaml-language-server",
-      "gopls",
+    -- lspconfig
+    {
+        "neovim/nvim-lspconfig",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            { "folke/neoconf.nvim", cmd = "Neoconf",                                config = true },
+            { "folke/neodev.nvim",  opts = { experimental = { pathStrict = true } } },
+            "mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+            {
+                "hrsh7th/cmp-nvim-lsp",
+                cond = function()
+                    return require("lazyvim.util").has("nvim-cmp")
+                end,
+            },
+        },
+        ---@class PluginLspOpts
+        opts = {
+            -- options for vim.diagnostic.config()
+            diagnostics = {
+                underline = true,
+                update_in_insert = false,
+                virtual_text = { spacing = 4, prefix = "●" },
+                severity_sort = true,
+            },
+            -- Automatically format on save
+            autoformat = true,
+            -- options for vim.lsp.buf.format
+            -- `bufnr` and `filter` is handled by the LazyVim formatter,
+            -- but can be also overridden when specified
+            format = {
+                formatting_options = nil,
+                timeout_ms = nil,
+            },
+            -- LSP Server Settings
+            ---@type lspconfig.options
+            servers = {
+                jsonls = {},
+                lua_ls = {
+                    -- mason = false, -- set to false if you don't want this server to be installed with mason
+                    settings = {
+                        Lua = {
+                            workspace = {
+                                checkThirdParty = false,
+                            },
+                            completion = {
+                                callSnippet = "Replace",
+                            },
+                        },
+                    },
+                },
+            },
+            -- you can do any additional lsp server setup here
+            -- return true if you don't want this server to be setup with lspconfig
+            ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+            setup = {
+                -- example to setup with typescript.nvim
+                -- tsserver = function(_, opts)
+                --   require("typescript").setup({ server = opts })
+                --   return true
+                -- end,
+                -- Specify * to use this function as a fallback for any server
+                -- ["*"] = function(server, opts) end,
+                omnisharp = function(server, opts)
+                    opts.on_attach = function(client, bufnr)
+                        client.server_capabilities.semanticTokensProvider = {
+                            full = vim.empty_dict(),
+                            legend = {
+                                tokenModifiers = { "static_symbol" },
+                                tokenTypes = {
+                                    "comment",
+                                    "excluded_code",
+                                    "identifier",
+                                    "keyword",
+                                    "keyword_control",
+                                    "number",
+                                    "operator",
+                                    "operator_overloaded",
+                                    "preprocessor_keyword",
+                                    "string",
+                                    "whitespace",
+                                    "text",
+                                    "static_symbol",
+                                    "preprocessor_text",
+                                    "punctuation",
+                                    "string_verbatim",
+                                    "string_escape_character",
+                                    "class_name",
+                                    "delegate_name",
+                                    "enum_name",
+                                    "interface_name",
+                                    "module_name",
+                                    "struct_name",
+                                    "type_parameter_name",
+                                    "field_name",
+                                    "enum_member_name",
+                                    "constant_name",
+                                    "local_name",
+                                    "parameter_name",
+                                    "method_name",
+                                    "extension_method_name",
+                                    "property_name",
+                                    "event_name",
+                                    "namespace_name",
+                                    "label_name",
+                                    "xml_doc_comment_attribute_name",
+                                    "xml_doc_comment_attribute_quotes",
+                                    "xml_doc_comment_attribute_value",
+                                    "xml_doc_comment_cdata_section",
+                                    "xml_doc_comment_comment",
+                                    "xml_doc_comment_delimiter",
+                                    "xml_doc_comment_entity_reference",
+                                    "xml_doc_comment_name",
+                                    "xml_doc_comment_processing_instruction",
+                                    "xml_doc_comment_text",
+                                    "xml_literal_attribute_name",
+                                    "xml_literal_attribute_quotes",
+                                    "xml_literal_attribute_value",
+                                    "xml_literal_cdata_section",
+                                    "xml_literal_comment",
+                                    "xml_literal_delimiter",
+                                    "xml_literal_embedded_expression",
+                                    "xml_literal_entity_reference",
+                                    "xml_literal_name",
+                                    "xml_literal_processing_instruction",
+                                    "xml_literal_text",
+                                    "regex_comment",
+                                    "regex_character_class",
+                                    "regex_anchor",
+                                    "regex_quantifier",
+                                    "regex_grouping",
+                                    "regex_alternation",
+                                    "regex_text",
+                                    "regex_self_escaped_character",
+                                    "regex_other_escape",
+                                },
+                            },
+                            range = true,
+                        }
+                    end
+                end
+            },
+        },
+        ---@param opts PluginLspOpts
+        config = function(plugin, opts)
+            -- setup autoformat
+            require("lazyvim.plugins.lsp.format").autoformat = opts.autoformat
+            -- setup formatting and keymaps
+            require("lazyvim.util").on_attach(function(client, buffer)
+                require("lazyvim.plugins.lsp.format").on_attach(client, buffer)
+                require("lazyvim.plugins.lsp.keymaps").on_attach(client, buffer)
+            end)
+
+            -- diagnostics
+            for name, icon in pairs(require("lazyvim.config").icons.diagnostics) do
+                name = "DiagnosticSign" .. name
+                vim.fn.sign_define(name, { text = icon, texthl = name, numhl = "" })
+            end
+            vim.diagnostic.config(opts.diagnostics)
+
+            local servers = opts.servers
+            local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+            local function setup(server)
+                local server_opts = vim.tbl_deep_extend("force", {
+                    capabilities = vim.deepcopy(capabilities),
+                }, servers[server] or {})
+
+                if opts.setup[server] then
+                    if opts.setup[server](server, server_opts) then
+                        return
+                    end
+                elseif opts.setup["*"] then
+                    if opts.setup["*"](server, server_opts) then
+                        return
+                    end
+                end
+                require("lspconfig")[server].setup(server_opts)
+            end
+
+            -- temp fix for lspconfig rename
+            -- https://github.com/neovim/nvim-lspconfig/pull/2439
+            local mappings = require("mason-lspconfig.mappings.server")
+            if not mappings.lspconfig_to_package.lua_ls then
+                mappings.lspconfig_to_package.lua_ls = "lua-language-server"
+                mappings.package_to_lspconfig["lua-language-server"] = "lua_ls"
+            end
+
+            local mlsp = require("mason-lspconfig")
+            local available = mlsp.get_available_servers()
+
+            local ensure_installed = {} ---@type string[]
+            for server, server_opts in pairs(servers) do
+                if server_opts then
+                    server_opts = server_opts == true and {} or server_opts
+                    -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
+                    if server_opts.mason == false or not vim.tbl_contains(available, server) then
+                        setup(server)
+                    else
+                        ensure_installed[#ensure_installed + 1] = server
+                    end
+                end
+            end
+
+            require("mason-lspconfig").setup({ ensure_installed = ensure_installed })
+            require("mason-lspconfig").setup_handlers({ setup })
+        end,
     },
-  },
+
+    -- formatters
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = { "mason.nvim" },
+        opts = function()
+            local nls = require("null-ls")
+            return {
+                sources = {
+                    -- nls.builtins.formatting.prettierd,
+                    nls.builtins.formatting.stylua,
+                    nls.builtins.diagnostics.flake8,
+                },
+            }
+        end,
+    },
+
+    -- cmdline tools and lsp servers
+    {
+
+        "williamboman/mason.nvim",
+        cmd = "Mason",
+        keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+        opts = {
+            ensure_installed = {
+                "json-lsp",
+                "lua-language-server",
+                "pyright",
+                "css-lsp",
+                "intelephense",
+                "jdtls",
+                "omnisharp",
+                "rust-analyzer",
+                "typescript-language-server",
+                "vetur-vls",
+                "yaml-language-server",
+                "gopls",
+            },
+        },
+        ---@param opts MasonSettings | {ensure_installed: string[]}
+        config = function(plugin, opts)
+            require("mason").setup(opts)
+            local mr = require("mason-registry")
+            for _, tool in ipairs(opts.ensure_installed) do
+                local p = mr.get_package(tool)
+                if not p:is_installed() then
+                    p:install()
+                end
+            end
+        end,
+    },
 }
